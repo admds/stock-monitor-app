eda312d825252edf8927a93351d73226f0914ba4
Mallory Solis <Mallory Solis>
2017-01-14 09:37:54 +0700

    Setup server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var ExpressServer = require('express-server');
+var winston = require('winston');
+
+var server = new ExpressServer({
+	name: 'My Express Server',
+	port: 3000,
+    requestLog: true,
+		logLevel: 'debug'
+});
+
+server.start(function() {
+	winston.info('Application is ready! Go to http://localhost:' + server.port);
+});
+
+process.on('SIGINT', function() {
+	server.stop();
+	process.exit();
+}.bind(this));
