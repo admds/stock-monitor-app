e023750e06539b7e48d817a2d6bbfc10a74e43db
Mallory Solis <mallie.solis@gmail.com>
2017-01-17 22:38:33 -0600

    Keeps company info in cookie and displays even when reloaded

--- client/add-stock.js
+++ client/add-stock.js
@@ -11,11 +11,22 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
 }])
 
 .controller('StockLookupCtrl', function($scope, $http, $cookies) {
+
+    $scope.oneAtATime = true;
     $scope.alerts = [];
+    $scope.groups = [];
+
     $scope.selectedSymbol = undefined;
+
     $scope.initialize = function() {
         $scope.stockCompanies = [];
         $scope.loadStockCompanies();
+
+        //If there are no displayed stocks, check cookies to display
+        if ($scope.groups.length == 0) {
+            $scope.displayWatchedStocks();
+        }
+
         console.log('StockLookupCtrl init');
     };
 
@@ -23,6 +34,16 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
         $http.get('/stock-info').then(function(response) {
             $scope.stockCompanies = response.data;
         });
+    }
+
+    $scope.displayWatchedStocks = function () {
+        var cookies = $cookies.getAll();
+
+        for(var property in cookies) {
+            if (property.indexOf('stock.') == 0) {
+                $scope.displayStockInfo(JSON.parse(cookies[property]), true);
+            }
+        }
     }
 
     $scope.submit = function() {
@@ -39,9 +60,15 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
                         symbol: $scope.selectedSymbol.ticker
                     }
                 }).then(function(response){
-                    //Setting cookie
-                    $cookies.put('stock.' + response.data.ticker, new Date());
-                    console.log(response);
+                    //Setting cookie and display information
+                    var stockInfo = {
+                        ticker : response.data.ticker,
+                        name : response.data.name,
+                        sector : response.data.sector,
+                        industry_category : response.data.industry_category,
+                        industry_group : response.data.industry_group
+                    };
+                    $scope.displayStockInfo(stockInfo, false);
                 });
             }
             //If watching - show an alert indicating that it is being watched already
@@ -50,6 +77,15 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
             }
         }
     };
+
+    //Display stock information
+    $scope.displayStockInfo = function(stockInfo, isLoading) {
+        if (!isLoading) {
+            $cookies.put('stock.' + stockInfo.ticker, JSON.stringify(stockInfo));
+        }
+
+        $scope.groups.push(stockInfo);
+    }
 
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
