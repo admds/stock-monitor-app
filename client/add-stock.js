4264e906d0462cf499425107917827fa22cc3cec
Mallory Solis <mallie.solis@gmail.com>
2017-01-20 21:45:19 -0600

    Added moving average information to display, with cookie edits

--- client/add-stock.js
+++ client/add-stock.js
@@ -10,8 +10,13 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
     });
 }])
 
-.controller('StockLookupCtrl', function($scope, $http, $cookies) {
-
+.controller('StockLookupCtrl', function($scope, $http, $cookies, $q) {
+    $scope.button = {
+        submit : {
+            text : "Submit",
+            loading : false
+        }
+    };
     $scope.oneAtATime = true;
     $scope.alerts = [];
     $scope.groups = [];
@@ -48,27 +53,48 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
     };
 
     $scope.submit = function() {
+        $scope.button.submit.text = "Please wait";
+        $scope.button.submit.loading = true;
+
         //Check if symbol was indeed entered into the field
         if ($scope.selectedSymbol.ticker) {
+            var stockInfo;
 
             //Check if already watching entered symbol
             var cookieExists = $cookies.get("stock." + $scope.selectedSymbol.ticker.toUpperCase());
 
             //If not watching - make the GET call to retrieve stock information
             if (!cookieExists) {
-                return $http.get('/information', {
+
+
+                var informationCall = $http.get('/information', {
                     params: {
                         symbol: $scope.selectedSymbol.ticker
                     }
-                }).then(function(response){
-                    //Setting cookie and display information
-                    var stockInfo = {
-                        ticker : response.data.ticker,
-                        name : response.data.name,
-                        sector : response.data.sector,
-                        industry_category : response.data.industry_category,
-                        industry_group : response.data.industry_group
+                });
+                var pricesCall = $http.get('/prices', {
+                    params: {
+                        symbol: $scope.selectedSymbol.ticker
+                    }
+                });
+                $q.all([informationCall, pricesCall]).then(function(arrayOfResults) {
+                    stockInfo = {
+                        ticker : arrayOfResults[0].data.ticker,
+                        name : arrayOfResults[0].data.name,
+                        sector : arrayOfResults[0].data.sector,
+                        industry_category : arrayOfResults[0].data.industry_category,
+                        industry_group : arrayOfResults[0].data.industry_group,
+                        movingAverages : {
+                            days15 : -1,
+                            days50 : -1,
+                            days100 : -1,
+                            days200 : -1
+                        }
                     };
+
+                    $scope.calcMovingAverages(arrayOfResults[1].data.data, stockInfo);
+
+                    console.log(stockInfo);
                     $scope.displayStockInfo(stockInfo, false);
                 });
             }
@@ -76,7 +102,28 @@ angular.module('stockMonitorApp.index', ['ngRoute', 'ngCookies', 'ngAnimate', 'n
                 $scope.alerts.push({msg: 'Already watching: ' + $scope.selectedSymbol.ticker.toUpperCase()});
             }
         }
+        $scope.button.submit.text = "Submit";
+        $scope.button.submit.loading = false;
     };
+
+    $scope.calcMovingAverages = function(arrOfDataPoints, stockInfo) {
+        console.log("Calculating moving averages: 15, 50, 100, 200");
+        var allClosePrices = arrOfDataPoints.map(function (dataPoint) {return dataPoint.close;})
+
+        stockInfo.movingAverages.days15 = $scope.calcMovingAverage(allClosePrices, 15);
+        stockInfo.movingAverages.days50 = $scope.calcMovingAverage(allClosePrices, 50);
+        stockInfo.movingAverages.days100 = $scope.calcMovingAverage(allClosePrices, 100);
+        stockInfo.movingAverages.days200 = $scope.calcMovingAverage(allClosePrices, 200);
+
+        return;
+    };
+
+    $scope.calcMovingAverage = function(allClosePrices, numDays) {
+        var closePrices = allClosePrices.slice(0, numDays);
+        var sum = closePrices.reduce(function(a, b) { return a + b; });
+        return (sum / numDays).toFixed(2);
+    };
+
 
     //Display stock information
     $scope.displayStockInfo = function(stockInfo, isLoading) {
